f8e619d3755ef915f3d9b4f3e25b7fd7e2e479aa
Chanakarn-TH <chanakarn.gigi@gmail.com>
2025-10-05 21:47:17 +0200

    week-03: draw static bird and pipes

--- src/flappybird.js
+++ src/flappybird.js
@@ -7,8 +7,8 @@ let context;
 //bird
 let birdWidth = 34; //width/height ratio = 408/228 = 17/12
 let birdHeight = 24;
-let birdX = boardWidth/8;
-let birdY = boardHeight/2;
+let birdX = boardWidth / 8;
+let birdY = boardHeight / 2;
 let birdImg;
 
 //pipes
@@ -20,22 +20,22 @@ let pipeY = 0;
 let topPipeImg;
 let bottomPipeImg;
 
-window.onload = function() {
-    board = document.getElementById("board");
-    board.height = boardHeight;
-    board.width = boardWidth;
-    context = board.getContext("2d"); //used for drawing on the board
+window.onload = function () {
+  board = document.getElementById("board");
+  board.height = boardHeight;
+  board.width = boardWidth;
+  context = board.getContext("2d"); //used for drawing on the board
 
-    //draw flappy bird
-    context.fillStyle = "green";
-    // Replace these with sensible numbers and experiment! :) e.g. context.fillRect(100, 100, 30, 50);
-    context.fillRect(Rectangular X Position, Rectangular Y Position, Rectangular Width, Rectangular Height);
+  //draw flappy bird
+  context.fillStyle = "green";
+  // Replace these with sensible numbers and experiment! :) e.g. context.fillRect(100, 100, 30, 50);
+  context.fillRect(50, 50, 18, 25);
 
-    //load images
-    birdImg = new Image();
-    birdImg.src = "./flappybird.png";
-    birdImg.onload = function() {
-      // Feel free to first experiment with some numbers: context.drawImage(birdImg, 100, 100, 30, 50) before using variables
-        context.drawImage(Variable that holds the image, Image X Position, Image Y Position, Image Width, Image Height);
-    }
-}
+  //load images
+  birdImg = new Image();
+  birdImg.src = "./flappybird.png";
+  birdImg.onload = function () {
+    // Feel free to first experiment with some numbers: context.drawImage(birdImg, 100, 100, 30, 50) before using variables
+    context.drawImage(birdImg, 34, 24, 8, 2);
+  };
+};
